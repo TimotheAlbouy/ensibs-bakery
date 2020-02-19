82b78a84d2bb24b4b35193ee2bdbdc3d16a27779
TimotheAlbouy <38692015+TimotheAlbouy@users.noreply.github.com>
2020-02-19 23:59:46 +0100

    fixed truc et truc

--- bakery-server/src/main/java/fr/ensibs/bakery/impl/OrdersServiceImpl.java
+++ bakery-server/src/main/java/fr/ensibs/bakery/impl/OrdersServiceImpl.java
@@ -1,39 +1,69 @@
 package fr.ensibs.bakery.impl;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
-import fr.ensibs.bakery.model.Order;
+import com.auth0.jwt.JWT;
+import fr.ensibs.bakery.model.*;
 import fr.ensibs.bakery.service.OrdersService;
 
+import javax.jws.WebService;
+
+/**
+ * A web service that manages customers' orders.
+ */
+@WebService(endpointInterface = "fr.ensibs.bakery.service.OrdersService",
+        serviceName = "OrdersService",
+        portName = "OrdersPort")
 public class OrdersServiceImpl implements OrdersService {
-	
-	/**
-	 * an order used by the user.
-	 */
-	private Order order;
-	
-	
-	@Override
-	public void addOrder() {
-		// TODO Auto-generated method stub	
-	}
-	
-	@Override
-	public Order createOrder() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
-	@Override
-	public ArrayList<Order> viewOrders() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    /**
+     * the DAO to handle orders in the database
+     */
+    private OrderDAO orderDAO;
 
-	@Override
-	public ArrayList<String> getList() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
+    /**
+     * the DAO to handle products in the database
+     */
+    private ProductDAO productDAO;
+
+    /**
+     * Constructor.
+     */
+    public OrdersServiceImpl() throws SQLException, ClassNotFoundException {
+        this.orderDAO = OrderDAO.getInstance();
+        this.productDAO = ProductDAO.getInstance();
+    }
+
+    @Override
+    public void addOrder(String token, int id, int quantity) {
+        // verify the token
+		User user = Auth.verify(token);
+		if (user == null)
+		    return;
+
+        Product product = this.productDAO.getProduct(id);
+
+		// if the user with the given id does not exist
+		if (product == null)
+			return;
+
+		this.orderDAO.createOrder(product.getId(), user.getId(), quantity);
+    }
+
+    @Override
+    public ArrayList<Order> getAllOrders(String token) {
+        // verify the token
+        User user = Auth.verify(token);
+        if (user == null)
+            return null;
+
+        return this.orderDAO.getAllOrders(user.getId());
+    }
+
+    @Override
+    public ArrayList<String> getAllProducts() {
+        return this.productDAO.getAllProducts();
+    }
+
 }
