fe4e4d794bd1563c7f9479093af35283f209981b
Gwenn <gwennquelo@hotmail.fr>
2020-02-21 11:47:41 +0100

    db

--- admin.sql
+++ admin.sql
@@ -1,3 +1,3 @@
 -- name = admin, password = admin
 INSERT INTO `User` (name, password_hash, role)
-VALUES ('admin', '$2y$10$0ElBVl54SNbyaA/AK7djbOAGtqCvxEPOw29NFDSRRCNPdlwr/Bcai', 'ADMIN');
+VALUES ('admin', '$2a$10$MkHrLTN.j6WjHU/IhqRQduw0PlspbXrhZa5XNICJngI7K9sXaJJ9.', 'ADMIN');
